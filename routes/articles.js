ab580a53fca3abaf2d8b5bb1a89fff98652874a0
Kaleb Avila <nt724405@iteso.mx>
2024-11-06 13:16:37 -0600

    herramientas

---
+++ routes/articles.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const articlesController = require('../controllers/articlesController.js');
+
+router.get('/', articlesController.getAll);
+router.get('/:id', articlesController.getById);
+router.post('/', articlesController.create);
+router.put('/:id', articlesController.update);
+router.delete('/:id', articlesController.remove);
+
+module.exports = router;
