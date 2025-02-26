a4f54f0b6df0ec27b6a33983b93adea54a7e88bd
ChristaBarrios <111098252+ChristaBarrios@users.noreply.github.com>
2025-02-26 11:19:00 -0600

    Rollback archivo tags.js

--- routes/tags.js
+++ routes/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const tagsController = require('../controllers/tagsControllers.js');
+const tagsController = require('../controllers/tagsController.js');
 
 router.get('/', tagsController.getAll);
 router.get('/:id', tagsController.getById);
