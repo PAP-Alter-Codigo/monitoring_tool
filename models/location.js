089157d61ca01364acc238b0a88a07189c0b299d
Hauske <dany.rams@hotmail.com>
2025-06-03 13:42:18 -0600

    Dynamoose implementation for Location

--- models/location.js
+++ models/location.js
@@ -1,110 +1,23 @@
-const { DynamoDBClient, GetItemCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand, ScanCommand } = require('@aws-sdk/client-dynamodb');
-const config = require('../config');
+const dynamoose = require("dynamoose");
 
-const client = new DynamoDBClient({
-    region: config.AWS_REGION,
-    credentials: {
-    accessKeyId: config.AWS_ACCESS_KEY,
-    secretAccessKey: config.AWS_SECRET_KEY
-}
+const locationSchema = new dynamoose.Schema({
+  _idLocation: {
+    type: Number,
+    hashKey: true,
+    default: () => Date.now(),
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  geolocation: {
+    type: Array,
+    schema: [Number],
+    required: true,
+    validate: (val) => val.length === 2
+  }
 });
 
-const LOCATION = 'Location';
+Location = dynamoose.model("Location", locationSchema);
 
-const getAll = async () => {
-    const params = {
-        TableName: LOCATION
-    };
-    const command = new ScanCommand(params);
-    const data = await client.send(command);
-    return data.Items;
-}
-
-const getById = async (id) => {
-    const params = {
-        TableName: LOCATION,
-        Key: {
-            "_idLocation": { N: id.toString() }
-        }
-    };
-    const command = new GetItemCommand(params);
-    const data = await client.send(command);
-    return data.Item;
-}
-
-const create = async (location) => {
-    const params = {
-        TableName: LOCATION,
-        Item: {
-            "_idLocation": { N: location._idLocation.toString() },
-            "name": { S: location.name },
-            "geolocation": {
-                L: [
-                    { N: location.geolocation[0].toString() },
-                    { N: location.geolocation[1].toString() }
-                ]
-            }
-        }
-    };
-    const command = new PutItemCommand(params);
-    await client.send(command);
-};
-
-const update = async (id, location) => {
-    const params = {
-        TableName: LOCATION,
-        Key: {
-            "_idLocation": { N: id.toString() }
-        },
-        UpdateExpression: 'SET #name = :name, #geolocation = :geolocation',
-        ExpressionAttributeNames: {
-            '#name': 'name',
-            '#geolocation': 'geolocation'
-        },
-        ExpressionAttributeValues: {
-            ':name': { S: location.name },
-            ':geolocation': location.geolocation
-                ? {
-                    L: [
-                        { N: location.geolocation[0].toString() },
-                        { N: location.geolocation[1].toString() }
-                    ]
-                }
-                : { L: [] }
-        }
-    };
-    const command = new UpdateItemCommand(params);
-    await client.send(command);
-};
-
-const remove = async (id) => {
-    // Primero verificamos si la ubicación existe antes de eliminarla
-    const getParams = {
-        TableName: LOCATION,
-        Key: { "_idLocation": { N: id.toString() } }
-    };
-
-    const getCommand = new GetItemCommand(getParams);
-    const existingLocation = await client.send(getCommand);
-
-    if (!existingLocation.Item) {
-        throw new Error(`Location con ID ${id} no encontrado.`);
-    }
-
-    // Si la ubicación existe, procedemos con la eliminación
-    const deleteParams = {
-        TableName: LOCATION,
-        Key: { "_idLocation": { N: id.toString() } }
-    };
-
-    const deleteCommand = new DeleteItemCommand(deleteParams);
-    await client.send(deleteCommand);
-};
-
-module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    remove
-};
+module.exports = Location;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dynamoose = require('dynamoose')
+const config = require('./config.js');
 
 const articlesRoutes = require('./routes/articleRoutes');
 const actorsRoutes = require('./routes/actorRoutes');
@@ -9,6 +11,15 @@ const locationsRoutes = require('./routes/locationRoutes');
 const app = express();
 
 app.use(bodyParser.json());
+
+const ddb = new dynamoose.aws.ddb.DynamoDB({
+  region: config.AWS_REGION,
+	credentials: {
+		accessKeyId: config.AWS_ACCESS_KEY,
+		secretAccessKey: config.AWS_SECRET_KEY
+	},
+});
+dynamoose.aws.ddb.set(ddb);
 
 app.use('/articles', articlesRoutes);
 app.use('/actors', actorsRoutes);
