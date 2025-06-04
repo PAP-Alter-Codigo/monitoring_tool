21ecdd6021a7b77b2a7a216a0e889ab322d0fd2e
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-06-04 11:13:31 -0600

    Dynamoose implementation for Articles

--- models/article.js
+++ models/article.js
@@ -1,108 +1,46 @@
-const { DynamoDBClient, GetItemCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand, ScanCommand, QueryCommand } = require('@aws-sdk/client-dynamodb');
-const config = require('../config');
+const dynamoose = require('dynamoose');
+const { v4: uuidv4 } = require('uuid');
 
-const client = new DynamoDBClient({
-  region: config.AWS_REGION,
-  credentials: {
-    accessKeyId: config.AWS_ACCESS_KEY,
-    secretAccessKey: config.AWS_SECRET_KEY
-  }
-});
-
-const ARTICLES = 'Articles';
-
-const getAll = async () => {
-  const command = new ScanCommand({ TableName: ARTICLES });
-  const data = await client.send(command);
-  return data.Items;
-};
-
-const getById = async (id, publicationDate) => {
-  const params = {
-    TableName: ARTICLES,
-    Key: {
-      _articleId: { N: id.toString() },
-      publicationDate: { S: publicationDate }
-    }
-  };
-  const command = new GetItemCommand(params);
-  const data = await client.send(command);
-  return data.Item;
-};
-
-const create = async (article) => {
-  const params = {
-    TableName: ARTICLES,
-    Item: {
-      _articleId: { N: article._articleId.toString() },
-      publicationDate: { S: article.publicationDate },
-      source: {
-        M: {
-          name: { S: article.source.name },
-          paywall: { BOOL: article.source.paywall },
-          headline: { S: article.source.headline },
-          url: { S: article.source.url },
-          author: { S: article.source.author },
-          coverageLevel: { S: article.source.coverageLevel }
-        }
-      },
-      actorsMentioned: { NS: article.actorsMentioned.map(String) },
-      tags: { NS: article.tags.map(String) },
-      geolocation: { N: article.geolocation.toString() }
-    }
-  };
-  const command = new PutItemCommand(params);
-  await client.send(command);
-};
-
-const update = async (id, publicationDate, article) => {
-  const params = {
-    TableName: ARTICLES,
-    Key: {
-      _articleId: { N: id.toString() },
-      publicationDate: { S: publicationDate }
+const articleSchema = new dynamoose.Schema({
+  id: {
+    type: String,
+    hashKey: true,
+    default: uuidv4
+  },
+  publicationDate: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: Object,
+    schema: {
+      name: String,
+      paywall: Boolean,
+      headline: String,
+      url: String,
+      author: String,
+      coverageLevel: String
     },
-    UpdateExpression: 'SET #source = :source, #actorsMentioned = :actors, #tags = :tags, #geolocation = :geo',
-    ExpressionAttributeNames: {
-      '#source': 'source',
-      '#actorsMentioned': 'actorsMentioned',
-      '#tags': 'tags',
-      '#geolocation': 'geolocation'
-    },
-    ExpressionAttributeValues: {
-      ':source': { M: {
-          name: { S: article.source.name },
-          paywall: { BOOL: article.source.paywall },
-          headline: { S: article.source.headline },
-          url: { S: article.source.url },
-          author: { S: article.source.author },
-          coverageLevel: { S: article.source.coverageLevel }
-        }},
-      ':actors': { NS: article.actorsMentioned.map(String) },
-      ':tags': { NS: article.tags.map(String) },
-      ':geo': { N: article.geolocation.toString() }
-    }
-  };
-  const command = new UpdateItemCommand(params);
-  await client.send(command);
-};
+    required: true
+  },
+  actorsMentioned: {
+    type: Array,
+    schema: [Number],
+    required: true,
+  },
+  tags: {
+    type: Array,
+    schema: [Number],
+    required: true,
+  },
+  geolocation: {
+    type: Number,
+    required: true,
+  },
+})
 
-const remove = async (id, publicationDate) => {
-  const params = {
-    TableName: ARTICLES,
-    Key: {
-      _articleId: { N: id.toString() },
-      publicationDate: { S: publicationDate } // Publication date part of the key
-    }
-  };
-  const command = new DeleteItemCommand(params);
-  await client.send(command);
-};
+Articles = dynamoose.model('Articles', articleSchema);
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  update,
-  remove
+  Articles
 };
