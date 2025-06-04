947ea861564b2926ec13165082e6432104d78b36
Hauske <dany.rams@hotmail.com>
2025-06-04 10:44:49 -0600

    Renaming for new table

--- models/location.js
+++ models/location.js
@@ -19,6 +19,6 @@ const locationSchema = new dynamoose.Schema({
   }
 });
 
-Location = dynamoose.model("Location", locationSchema);
+Location = dynamoose.model("Locations", locationSchema);
 
 module.exports = Location;
