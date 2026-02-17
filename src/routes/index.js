15d1298914faf1627af8c2ddb8e985d0de6da6fe
JorgeVillaFa <josejorgevillarreal@gmail.com>
2026-02-17 14:24:56 -0600

    feature/ A new routes file was created for dev, and a list of authorized emails was added.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const authRoutes = require('./authRoutes');
+const devAuthRoutes = require('./devAuthRoutes');
 const articlesRoutes = require('./articleRoutes');
 const actorsRoutes = require('./actorRoutes');
 const tagsRoutes = require('./tagRoutes');
 const locationsRoutes = require('./locationRoutes');
 const { authJwtCookie } = require('../middlewares/authJwt');
+const { adminAllowlist } = require("../middlewares/adminAllowList");
 
+const ADMIN_EMAILS = (process.env.ADMIN_EMAILS || "")
+  .split(",")
+  .map((s) => s.trim())
+  .filter(Boolean);
 
 router.use('/auth', authRoutes);
+router.use('/auth', devAuthRoutes); // dev routes
 
 //MIDDLEWARE
 router.use(authJwtCookie);
+
+router.use(
+  adminAllowlist({
+    allowedEmails: ADMIN_EMAILS,
+  })
+);
 
 router.use('/articles', articlesRoutes);
 router.use('/actors', actorsRoutes);
