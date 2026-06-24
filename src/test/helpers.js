a5b980453e7eb3da53e1a7252ddd81f7c1dbb798
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2026-06-23 19:41:04 -0600

    fix: update test paths and improve integration tests with mock authentication

---
+++ src/test/helpers.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+const authJwtModule = require('../middlewares/authJwt');
+const apiKeyModule = require('../middlewares/apiKey');
+const adminAllowlistModule = require('../middlewares/adminAllowList');
+
+function createTestToken(email = 'test@admin.com', name = 'Test Admin') {
+  const payload = {
+    sub: email,
+    email,
+    name,
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET || 'test-secret', {
+    expiresIn: '15m',
+  });
+}
+
+function mockAuthMiddleware(sandbox, adminEmail = 'test@admin.com') {
+  // Mock authJwtCookie to auto-authenticate
+  sandbox.stub(authJwtModule, 'authJwtCookie').callsFake((req, res, next) => {
+    req.user = {
+      sub: adminEmail,
+      email: adminEmail,
+      name: 'Test Admin',
+      roles: [],
+    };
+    next();
+  });
+
+  // Mock apiKeyAuth
+  sandbox.stub(apiKeyModule, 'apiKeyAuth').callsFake((req, res, next) => {
+    req.user = { email: 'lambda-service', roles: ['service'] };
+    next();
+  });
+
+  // Mock adminAllowlist to always allow
+  sandbox.stub(adminAllowlistModule, 'adminAllowlist').returns((req, res, next) => {
+    next();
+  });
+}
+
+module.exports = {
+  createTestToken,
+  mockAuthMiddleware,
+};
